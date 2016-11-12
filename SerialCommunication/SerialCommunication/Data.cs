75085c64acaa37a4e9b540446c981456ba2f2374
Larcele <janka.harvanova@gmail.com>
2016-11-12 19:36:05 +0100

    prototyp triedy Data, ktora bude serializovana do databazy

---
+++ SerialCommunication/SerialCommunication/Data.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialCommunication
+{
+    public class Data
+    {
+        public ushort Day { get; set; }
+        public ushort Month { get; set; }
+        public ushort Year { get; set; }
+        public ushort ProbeID { get; set; }
+        public double Value { get; set; }
+
+        public Data(ushort sondaID, double sondaValue, string timestamp)
+        {
+            //Todo rozparsovat datum, teraz sa mi nechce
+            ProbeID = sondaID;
+            Value = sondaValue;
+        }
+    }
+}
