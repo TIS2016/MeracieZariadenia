748f2ec7ab4cb706f4961be94a5e4504f9ef4a8d
Sam <Sam>
2016-12-08 23:48:44 +0100

    Pridane grafy s nahodne generovanymi datami

--- Web/index.php
+++ Web/index.php
@@ -20,6 +20,11 @@ if (isset($_GET['a']) && $_GET['a'] == 'get_data') {
 echo '<html>
     <head>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="css/chartist.css">
+        <link rel="stylesheet" href="css/chartist-plugin-tooltip.css">
+        <link rel="stylesheet" href="scss/chartist-plugin-tooltip.scss">
+        <script src="https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
+        <script src="scripts/chartist-plugin-tooltip.js"></script>
 
         <!-- HERE LOAD JQUERY GRAPH -->
         <script type="text/javascript">
@@ -64,6 +69,43 @@ echo '<html>
     </form>
     
     <div class="result">
+        <h1> TESTOVACIE GRAFY MERANI </h1>
+    <div class="ct-chart" id="chart1" style="margin-top: 120px;height: 300px;"></div>
+    <div style="margin-bottom: 120px;"></div>
+    <div class="ct-chart" id="chart2" style="height: 300px;"></div>
+    <script>
+        pole1count = [];
+        pole1values = [];
+        pole2values = [];
+        for ($i=1;$i<100;$i++) {
+            if ($i%10==0) pole1count.push(($i/10)+".12.2016");
+            else pole1count.push(" ");
+            pole1values.push(Math.floor((Math.random() * 10) + 60)/100);
+            pole2values.push(Math.floor((Math.random() * 10) + 30)/100);
+        }
+        // Initialize a Line chart in the container with the ID chart1
+        new Chartist.Line("#chart1", {
+        labels: pole1count,
+        series: [pole1values]
+        }, {
+            plugins: [
+                Chartist.plugins.tooltip()
+            ]
+        });
+    </script>
+    <script>
+        console.log(pole1values);
+
+        // Initialize a Line chart in the container with the ID chart2
+        new Chartist.Line("#chart2", {
+        labels: pole1count,
+        series: [pole2values]
+        }, {
+            plugins: [
+                Chartist.plugins.tooltip()
+            ]
+        });
+    </script>
     </div>
 </body>
 </html>';
