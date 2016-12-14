20b3103fb6e8dc36a90572965085df21e8aceedf
Sam <Sam>
2016-12-14 12:11:43 +0100

    vykreslovanie udajov pomocou highcharts #25

--- Web/index.php
+++ Web/index.php
@@ -1,7 +1,6 @@
 <?php
 $dateFrom = date('Y-m-d',strtotime("-7 day"));
 $dateTo = date("Y-m-d");
-
 function get_data($od, $do) {
     $value = [
         $od,
@@ -9,30 +8,26 @@ function get_data($od, $do) {
     ];
     return json_encode($value, JSON_FORCE_OBJECT);
 }
-
 if (isset($_GET['a']) && $_GET['a'] == 'get_data') {
     $od = $_GET['od']? : $dateTo;
     $do = $_GET['do']? : $dateTo;
     echo get_data($od, $do);
     return;
 }
-
 echo '<html>
     <head>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <link rel="stylesheet" href="css/chartist.css">
-        <link rel="stylesheet" href="css/chartist-plugin-tooltip.css">
-        <link rel="stylesheet" href="scss/chartist-plugin-tooltip.scss">
-        <script src="https://cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
-        <script src="scripts/chartist-plugin-tooltip.js"></script>
-
         <!-- HERE LOAD JQUERY GRAPH -->
         <script type="text/javascript">
             $(function() {
                 
                 // here initialize grap
                 
-            
+                date = [1,2,3];
+                values = [1,2,3];
+                date1 = new Date();
+                date2 = new Date();
+
                 // initialize form handler
                 $("form").on("submit", function(e) {
                     e.preventDefault();
@@ -49,13 +44,112 @@ echo '<html>
                             $(".result").html("");
                             $.each(result, function(key,value) {
                                 $(".result").append("<p><b>" + key + "</b>: " + value + "</p>");
+                                if (key==0) {
+                                    date1 = value;
+                                }
+                                else date2 = value;
                             });
+                            date1 = new Date(date1);
+                            date2 = new Date(date2);
+                            date = [];
+                            values  = [];
+                            while (date1<=date2) {
+                                date1 = new Date(date1);
+                                result = date1.toISOString().substring(0,10);
+                                date.push(result);
+                                values.push(Math.floor((Math.random() * 10) + 60)/100);
+                                //$(".result").append("<p>" + result + "</p>");
+                                date1.setDate( date1.getDate() + 1 );                        
+                            }
+                            var chart = Highcharts.chart("container", {
+
+                                title: {
+                                    text: "Meranie {INSERT NAME}"
+                                },
+
+                                xAxis: {
+                                    //tickInterval: 14 * 24 * 3600 * 1000, // two weeks
+                                    tickWidth: 0,
+                                    categories: date,
+                                    gridLineWidth: 1,
+                                    labels: {
+                                        align: "left",
+                                        x: 3,
+                                        y: -3
+                                    }
+                                },
+
+                                yAxis: [{ // left y axis
+                                    title: {
+                                        text: null
+                                    },
+                                    labels: {
+                                        align: "left",
+                                        x: 3,
+                                        y: 16,
+                                        format: "{value:.,0f}"
+                                    },
+                                    showFirstLabel: false
+                                }, { // right y axis
+                                    linkedTo: 0,
+                                    gridLineWidth: 0,
+                                    opposite: true,
+                                    title: {
+                                        text: null
+                                    },
+                                    labels: {
+                                        align: "right",
+                                        x: -3,
+                                        y: 16,
+                                        format: "{value:.,0f}"
+                                    },
+                                    showFirstLabel: false
+                                }],
+
+                                legend: {
+                                    align: "left",
+                                    verticalAlign: "top",
+                                    y: 20,
+                                    floating: true,
+                                    borderWidth: 0
+                                },
+
+                                tooltip: {
+                                    shared: true,
+                                    crosshairs: true
+                                },
+
+                                plotOptions: {
+                                    series: {
+                                        cursor: "pointer",
+                                        point: {
+                                            events: {
+
+                                            }
+                                        },
+                                        marker: {
+                                            lineWidth: 1
+                                        }
+                                    }
+                                },
+
+                                series: [{
+                                    name: "Hodnota1",
+                                    data: values,
+                                    lineWidth: 4,
+                                    marker: {
+                                        radius: 4
+                                    }
+                                }]
+                            });
+
                         },
                         error: function() {
                             console.log("error");
                         }
                     });
                 });
+
             });
         </script>
     </head>
@@ -69,43 +163,16 @@ echo '<html>
     </form>
     
     <div class="result">
-        <h1> TESTOVACIE GRAFY MERANI </h1>
-    <div class="ct-chart" id="chart1" style="margin-top: 120px;height: 300px;"></div>
-    <div style="margin-bottom: 120px;"></div>
-    <div class="ct-chart" id="chart2" style="height: 300px;"></div>
-    <script>
-        pole1count = [];
-        pole1values = [];
-        pole2values = [];
-        for ($i=1;$i<100;$i++) {
-            if ($i%10==0) pole1count.push(($i/10)+".12.2016");
-            else pole1count.push(" ");
-            pole1values.push(Math.floor((Math.random() * 10) + 60)/100);
-            pole2values.push(Math.floor((Math.random() * 10) + 30)/100);
-        }
-        // Initialize a Line chart in the container with the ID chart1
-        new Chartist.Line("#chart1", {
-        labels: pole1count,
-        series: [pole1values]
-        }, {
-            plugins: [
-                Chartist.plugins.tooltip()
-            ]
-        });
-    </script>
-    <script>
-        console.log(pole1values);
-
-        // Initialize a Line chart in the container with the ID chart2
-        new Chartist.Line("#chart2", {
-        labels: pole1count,
-        series: [pole2values]
-        }, {
-            plugins: [
-                Chartist.plugins.tooltip()
-            ]
-        });
-    </script>
     </div>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/data.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+
+    <!-- Additional files for the Highslide popup effect -->
+    <script src="https://www.highcharts.com/samples/static/highslide-full.min.js"></script>
+    <script src="https://www.highcharts.com/samples/static/highslide.config.js" charset="utf-8"></script>
+    <link rel="stylesheet" type="text/css" href="https://www.highcharts.com/samples/static/highslide.css" />
+
+    <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
 </body>
 </html>';
