b328a7d07ca2ab8c1f7fc1073f5cfa3caf4eb0db
Samuel <samwendl1@gmail.com>
2017-01-20 17:28:26 +0100

    Uprava zobrazovania grafov na stranke #25

--- Web/index.php
+++ Web/index.php
@@ -1,27 +1,33 @@
 <?php
 $dateFrom = date('Y-m-d',strtotime("-7 day"));
 $dateTo = date("Y-m-d");
-function get_data($od, $do) {
+
+function get_data($od, $do, $sonda_id) {
     $value = [
         $od,
         $do,
+        $sonda_id
     ];
     return json_encode($value, JSON_FORCE_OBJECT);
 }
 if (isset($_GET['a']) && $_GET['a'] == 'get_data') {
     $od = $_GET['od']? : $dateTo;
     $do = $_GET['do']? : $dateTo;
-    echo get_data($od, $do);
+    $sonda_id = $_GET['sonda_id'];
+    echo get_data($od, $do, $sonda_id);
     return;
 }
 echo '<html>
     <head>
+         <meta http-equiv="Content-Language" content="sk" />
+         <meta charset="UTF-8">
+
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <!-- HERE LOAD JQUERY GRAPH -->
         <script type="text/javascript">
             $(function() {
                 
-                // here initialize grap
+                // here initialize grapz
                 
                 date = [1,2,3];
                 values = [1,2,3];
@@ -31,28 +37,42 @@ echo '<html>
                 // initialize form handler
                 $("form").on("submit", function(e) {
                     e.preventDefault();
-                    
+
                     $.ajax({
                         type: "get",
                         url: "index.php?a=get_data",
                         data: {
                             od : $(this).find("[name=od]").val(),
-                            do : $(this).find("[name=do]").val()
+                            do : $(this).find("[name=do]").val(),
+                            sonda_id : $(this).attr("id") 
                         },
                         dataType: "json",
                         success: function(result) {
                             $(".result").html("");
                             $.each(result, function(key,value) {
-                                $(".result").append("<p><b>" + key + "</b>: " + value + "</p>");
+
                                 if (key==0) {
                                     date1 = value;
                                 }
-                                else date2 = value;
+                                else if (key==1) date2 = value;
                             });
-                            date1 = new Date(date1);
-                            date2 = new Date(date2);
+
                             date = [];
                             values  = [];
+                            graph_element = "";
+                            name = "";
+                            if (result[2] == "sonda1") {
+                                graph_element="container";
+                                name="Sonda 1";
+                            }
+                            else {
+                                graph_element="container2";
+                                name="Sonda 2";
+                            }
+                            // naplnanie random datami
+                            date1 = new Date(date1);
+                            date2 = new Date(date2);
+
                             while (date1<=date2) {
                                 date1 = new Date(date1);
                                 result = date1.toISOString().substring(0,10);
@@ -61,7 +81,8 @@ echo '<html>
                                 //$(".result").append("<p>" + result + "</p>");
                                 date1.setDate( date1.getDate() + 1 );                        
                             }
-                            var chart = Highcharts.chart("container", {
+                            // koniec naplnania random datami
+                            var chart = Highcharts.chart(graph_element, {
 
                                 chart: {
                                     type: "line",
@@ -69,14 +90,14 @@ echo '<html>
                                 },
 
                                 title: {
-                                    text: "Meranie {INSERT NAME}"
+                                    text: "Meranie "+name
                                 },
 
 
                                 xAxis: {
                                     //tickInterval: 14 * 24 * 3600 * 1000, // two weeks
                                     tickWidth: 0,
-                                    categories: date,
+                                    categories: date, // sem vkladam datumy
                                     gridLineWidth: 1,
                                     labels: {
                                         align: "left",
@@ -141,7 +162,7 @@ echo '<html>
 
                                 series: [{
                                     name: "Hodnota1",
-                                    data: values,
+                                    data: values, // sem vkladam hodnoty
                                     lineWidth: 4,
                                     marker: {
                                         radius: 4
@@ -159,26 +180,47 @@ echo '<html>
             });
         </script>
     </head>
-<body>
-    <form>
-        OD:<br />
-        <input type="date" name="od" value="' . $dateFrom  . '" /> <br />
-        DO:<br />
-        <input type="date" name="do" value="' . $dateTo . '" /> <br />
-        <button type="submit" name="a" value="get_data" >Load data</button>
-    </form>
-    
-    <div class="result">
+<body style="text-align: center;">
+    <div style="margin-top: 30px">
+        <h1> Výpis údajov dvoch sond meracieho zariadenia na grafoch </h1>
+        <form id="sonda1" style="margin-top: 60px">
+            <h2> Sonda 1 </h2>
+            <b>OD:</b>
+            <input type="time" name="odCas" value="00:00" /> 
+            <input type="date" name="od" value="' . $dateFrom  . '" /> 
+            <b style="margin-left: 50px;">DO:</b>
+            <input type="time" name="doCas" value="00:00" /> 
+            <input type="date" name="do" value="' . $dateTo . '" /> 
+            <button type="submit" name="a" value="get_data" >Načítaj data</button>
+        </form>
+        
+        <div class="result">
+        </div>
+
+        <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+
+        <form id="sonda2" style="margin-top: 60px">
+            <h2> Sonda 2 </h2>
+            <b>OD:</b>
+            <input type="time" name="odCas" value="00:00" /> 
+            <input type="date" name="od" value="' . $dateFrom  . '" /> 
+            <b style="margin-left: 50px;">DO:</b>
+            <input type="time" name="doCas" value="00:00" /> 
+            <input type="date" name="do" value="' . $dateTo . '" /> 
+            <button type="submit" name="a" value="get_data" >Načítaj data</button>
+        </form>
+
+        <div id="container2" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+
+        <script src="https://code.highcharts.com/highcharts.js"></script>
+        <script src="https://code.highcharts.com/modules/data.js"></script>
+        <script src="https://code.highcharts.com/modules/exporting.js"></script>
+
+        <!-- Additional files for the Highslide popup effect -->
+        <script src="https://www.highcharts.com/samples/static/highslide-full.min.js"></script>
+        <script src="https://www.highcharts.com/samples/static/highslide.config.js" charset="utf-8"></script>
+        <link rel="stylesheet" type="text/css" href="https://www.highcharts.com/samples/static/highslide.css" />
+
     </div>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/modules/data.js"></script>
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
-
-    <!-- Additional files for the Highslide popup effect -->
-    <script src="https://www.highcharts.com/samples/static/highslide-full.min.js"></script>
-    <script src="https://www.highcharts.com/samples/static/highslide.config.js" charset="utf-8"></script>
-    <link rel="stylesheet" type="text/css" href="https://www.highcharts.com/samples/static/highslide.css" />
-
-    <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
 </body>
 </html>';
